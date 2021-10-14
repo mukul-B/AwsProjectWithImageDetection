bcdb74e22cc7cd8675ab212605845e548b0da0a9
mukul-B <mukulbadhan@gmail.com>
2021-10-14 12:47:19 -0700

    integrating s3 code

--- src/main/java/Instance1.java
+++ src/main/java/Instance1.java
@@ -1,9 +1,22 @@
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.sqs.SqsClient;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import java.util.List;
 
 public class Instance1 {
 
     public static void main(String[] args) {
+
+        S3 s3= new S3();
+
+        List<String> labels= s3.ListObject();
+
+        for (String os : labels) {
+            System.out.println("* " +os);
+            s3.GetObject(os);
+            break;
+
+        }
+
 
     }
 }
---
+++ src/main/java/S3.java
@@ -0,0 +1,118 @@
+//snippet-sourcedescription:[S3.java demonstrates how to list objects within an Amazon S3 bucket.]
+//snippet-keyword:[Java]
+//snippet-sourcesyntax:[java]
+//snippet-keyword:[Code Sample]
+//snippet-keyword:[Amazon S3]
+//snippet-keyword:[listObjectsV2]
+//snippet-service:[s3]
+//snippet-sourcetype:[full-example]
+//snippet-sourcedate:[]
+//snippet-sourceauthor:[soo-aws]
+/*
+   Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * List objects within an Amazon S3 bucket.
+ *
+ * This code expects that you have AWS credentials set up per:
+ * http://docs.aws.amazon.com/java-sdk/latest/developer-guide/setup-credentials.html
+ */
+public class S3 {
+    List<String>  ListObject() {
+        final String USAGE = "\n" +
+                "To run this example, supply the name of a bucket to list!\n" +
+                "\n" +
+                "Ex: ListObjects <bucket-name>\n";
+
+//        if (args.length < 1) {
+//            System.out.println(USAGE);
+//            System.exit(1);
+//        }
+
+        //String bucket_name = args[0];
+        String bucket_name = "unr-cs442";
+
+        System.out.format("Objects in S3 bucket %s:\n", bucket_name);
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+        ListObjectsV2Result result = s3.listObjectsV2(bucket_name);
+        List<S3ObjectSummary> objects = result.getObjectSummaries();
+        List<String> labels =new ArrayList<String>();
+        for (S3ObjectSummary os : objects) {
+
+            labels.add(os.getKey());
+        }
+        return labels;
+    }
+
+    public void GetObject(String key_name) {
+
+        final String USAGE = "\n" +
+                "To run this example, supply the name of an S3 bucket and object to\n" +
+                "download from it.\n" +
+                "\n" +
+                "Ex: GetObject <bucketname> <filename>\n";
+
+//        if (args.length < 2) {
+//            System.out.println(USAGE);
+//            System.exit(1);
+//        }
+
+        String bucket_name = "unr-cs442";
+      //  String key_name = "10.jpeg";
+
+        System.out.format("Downloading %s from S3 bucket %s...\n", key_name, bucket_name);
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+        try {
+            System.out.println("hello");
+            S3Object o = s3.getObject(bucket_name, key_name);
+            System.out.println("hello");
+            S3ObjectInputStream s3is = o.getObjectContent();
+            FileOutputStream fos = new FileOutputStream(new File("output/"+key_name));
+            byte[] read_buf = new byte[1024];
+            int read_len = 0;
+            while ((read_len = s3is.read(read_buf)) > 0) {
+                fos.write(read_buf, 0, read_len);
+            }
+            s3is.close();
+            fos.close();
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+        System.out.println("Done!");
+
+    }
+}
